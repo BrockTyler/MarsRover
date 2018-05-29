0cde8a29f9b812d6917c6d334dcb21091a476eff
BrockTyler <35366483+BrockTyler@users.noreply.github.com>
2018-05-29 12:25:15 -0600

    Initial Commit

---
+++ MarsRover/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MarsRover
+//
+//  Created by brock tyler on 5/29/18.
+//  Copyright © 2018 Brock Tyler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MarsRover/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MarsRover
+//
+//  Created by brock tyler on 5/29/18.
+//  Copyright © 2018 Brock Tyler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
